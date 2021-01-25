1124fd55312af8ff9df0960775871c3b127bb9b3
Takahiro Ueda <tkhr6022@gmail.com>
2021-01-26 08:28:07 +0900

    UPDATE

--- src/views/New.vue
+++ src/views/New.vue
@@ -1,38 +1,31 @@
 <template>
     <div class="editor">
-        <h1>New Memo</h1>
+        <Header>New Memo</Header>
         <textarea name="memo" v-model="memoBody"></textarea>
         <button @click="save">保存</button>
     </div>
 </template>
 
-<style scoped>
-textarea {
-    width: 100%;
-    height: 10em;
-}
-button {
-    border: 1px solid #333;
-    background-color: #333;
-    color: #fff;
-    padding: 10px 20px;
-    margin-top: 10px;
-}
-</style>
-
 <script>
+import Header from "@/components/Header.vue"
+
 export default {
     name: "new",
+    components: {
+        Header
+    },
     data: function() {
         return {
             memoBody: ""
         }
     },
     methods: {
-    save: function() {
-        alert('保存されました');
-        this.$router.push("/");
-        } 
+        save: function() {
+            this.$store.commit("save", {
+                body: this.memoBody
+            });
+            this.$router.push("/");
+        }
     }
 };
 </script>
